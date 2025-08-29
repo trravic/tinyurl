b8600f6edeb162948efa5fd9b711568d78dbbd92
VaibhavKVerma <vaibhavverma15062001@gmail.com>
2025-08-29 11:51:33 +0530

    Dockerize container

--- tokenservice/src/main/java/com/shorturl/tokenservice/config/RedisConfig.java
+++ tokenservice/src/main/java/com/shorturl/tokenservice/config/RedisConfig.java
@@ -1,16 +1,25 @@
 package com.shorturl.tokenservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new JedisConnectionFactory(config);
     }
 
     @Bean
